12ab178077f1238de7ecaffcf2fe9cea15eca6f1
Jose F <alu0100894646@ull.edu.es>
2019-11-01 18:50:22 +0000

    Estructura con el Grid y icono

--- pages/Index.js
+++ pages/Index.js
@@ -1,13 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 
-const Index = () => (
-    <div>
-        <p> Aqui va los stats</p>
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function FullWidthGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <AccountCircleIcon /> registro
+          <Paper className={classes.paper}>Aqui Irian las STATS</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>El video game</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper className={classes.paper}>Los objetos o mejoras</Paper>
+        </Grid>
+      </Grid>
     </div>
-    <div>
-        <p> aqui va el juego</p>
-    </div>
-    <div>
-        <p> aqui va las mejoras</p>
-    </div>
-)
+  );
+}
