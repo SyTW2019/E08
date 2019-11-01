f5b93a299d78a6e6b4f42bbec38a462f7b2e2b2f
Jose F <alu0100894646@ull.edu.es>
2019-11-01 18:12:53 +0000

    Estructura con Divs

---
+++ pages/Index.js
@@ -0,0 +1,13 @@
+
+
+const Index = () => (
+    <div>
+        <p> Aqui va los stats</p>
+    </div>
+    <div>
+        <p> aqui va el juego</p>
+    </div>
+    <div>
+        <p> aqui va las mejoras</p>
+    </div>
+)
