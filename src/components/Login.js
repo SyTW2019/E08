310e11dea0ea4e68e66dc4afadc0d4b227660de4
alu0100882339 <alu0100882339@ull.edu.es>
2019-11-21 21:29:31 +0000

    Login mejorado, falta arreglar lo del error

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,256 +14,171 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 class Login extends React.Component{
 
-  state = {
-    emailValue: "",
-    pwdValue: "",
-  }
+    constructor(props){
+        super(props);
 
-  error_state = {
-    emailError: "",
-    pwdError: "",
-  }
-
-  dialog = {
-    open:"",
-  }
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-
-  handleError = event => {
-    this.setState({
-      [event.target.name]: event.taget.value
-    })
-  }
-
-  handleOpen = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-  handleSubmit = event => {
-    event.preventDefault()
-    this.props.userPostFetch(this.state);
-  }
-
-  validateEmail = () => {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.error_state.emailValue))
-    {
-      this.error_state.setState({emailError: false})
-      //setEmailError(false);
-      return true;
-    }
-    else 
-    {
-      this.error_state.setState({emailError: true})
-      //setEmailError(true);
-      return false;
-    }
-  }
-
-  get_value = () => {
-    if(this.validateEmail())
-        if(this.state.emailValue)
-        {
-          this.error_state.setState({emailError: false})
-          //setPwdError(false);
-          console.log("Trying to log in")
-          //handleClose()
+        this.state = {
+            emailValue: "",
+            pwdValue: "",
+            open: "",
+            emailError: "",
+            pwdError: "",
         }
-        else
-          this.error_state.setState({emailError: true})
-          //setPwdError(true);
-  }
 
-  render() {
-    return(
-      <Grid>
-        <Button variant="outlined" color="primary" name='open' onClick={this.handleOpen}>
-        <AccountCircle />
-          iniciar sesión
-        </Button>
-        <Dialog
-          name='open'
-          keepMounted
-          onClose={this.handleOpen}
-          aria-labelledby="iniciar_sesion"
-          aria-describedby="alert-dialog-slide-description"
-        >
-          <DialogTitle id="iniciar_sesion">{"Inicio de sesión"}</DialogTitle>
-          <DialogContent>
-          <Grid>
-          <TextField
-            required
-            id="email"
-            name='emailError'
-            label="Email"
-            value={this.state.emailValue}
-            onChange={this.handleChange}
-            error={this.handleError}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              ),
-            }}
-          />
-          </Grid>
-          <Grid>
-          <TextField
-            required
-            id="password"
-            label="Contraseña"
-            type="password"
-            name='pwdError'
-            value={this.state.pwdValue}
-            onChange={this.handleChange}
-            error={this.handleError}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <LockOutlinedIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-          </Grid>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleOpen} color="primary">
-              Cancelar
-            </Button>
-            <Button onClick={this.get_values} color="primary">
-              Continuar
-            </Button>
-          </DialogActions>
-          </Dialog>
-        </Grid>
-    )
-  }
-} 
+        /*this.error_state = {
+            emailError: "",
+            pwdError: "",
+        }*/
 
-export default Login;
+        /*this.dialog = {
+            open: "",
+        }*/
 
-/*const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
-});
-const useStyles = makeStyles(theme => ({
-  margin: {
-    margin: theme.spacing(1),
-  },
-}));
+        this.handleChange = this.handleChange.bind(this);
+        this.handleError = this.handleError.bind(this);
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
 
-export default function AlertDialogSlide() {
-  const [open, setOpen] = React.useState(false);
-  //Variables para coger datos del formulario
-  const [emailValue, setEmailValue] = React.useState('');
-  const [pwdValue, setPwdValue] = React.useState('');
-  const [emailError, setEmailError] = React.useState(false);
-  const [pwdError, setPwdError] = React.useState(false);
-
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const validateEmail = () => {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailValue))
-    {
-      setEmailError(false);
-      return true;
     }
-    else 
-    {
-      setEmailError(true);
-      return false;
-    }
-  }
 
-  const get_values = () => {
-      if(validateEmail())
-        if(pwdValue)
-        {
-          setPwdError(false);
-          console.log("Trying to log in")
-          //handleClose()
-        }
-        else
-          setPwdError(true);
+    handleChange = event => {
+        event.preventDefault();
+        
+        const target = event.target;
+        const value = target.value;
+        const name = target.name
+        this.setState({
+          [name]: value
+        });
+        console.log("en el handleChange " + event.target.value)
+      }
     
-  }
+      handleError = event => {
+        this.setState({
+          [event.target.name]: event.taget.value
+        })
+        console.log("en el handleError")
+      }
+    
+      handleOpen = event => {
+        this.setState({
+          open: true,
+        })
+        
+        
 
-  //TO-DO => Función pa conectarse con el servidor 
+      }
+      handleClose = event => {
+        this.setState({
+          open: false,
+        })
+      }
+    
+      handleSubmit = event => {
+        event.preventDefault()
+        
+        this.props.userPostFetch(this.state);
+      }
 
-  return (
-    <Grid>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-      <AccountCircle />
-        iniciar sesión
-      </Button>
-      <Dialog
-        open={open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={handleClose}
-        aria-labelledby="iniciar_sesion"
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle id="iniciar_sesion">{"Inicio de sesión"}</DialogTitle>
-        <DialogContent>
-        <Grid>
-        <TextField
-          required
-          id="email"
-          label="Email"
-          value={emailValue}
-          onChange={(e)=> setEmailValue(e.target.value)}
-          error={emailError}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-            ),
-          }}
-        />
-        </Grid>
-        <Grid>
-        <TextField
-          required
-          id="password"
-          label="Contraseña"
-          type="password"
-          value={pwdValue}
-          onChange={(e)=> setPwdValue(e.target.value)}
-          error={pwdError}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockOutlinedIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-        </Grid>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancelar
-          </Button>
-          <Button onClick={get_values} color="primary">
-            Continuar
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </Grid>
-  );
-}*/
+      validateEmail = () => {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.emailValue))
+        {
+          this.setState({emailError: false})
+          //setEmailError(false);
+          return true;
+        }
+        else 
+        {
+          this.setState({emailError: true})
+          //setEmailError(true);
+          return false;
+        }
+      }
+    
+      get_value = () => {
+        console.log("en get_values")
+        if(this.validateEmail())
+            if(this.state.emailValue)
+            {
+              this.setState({emailError: false})
+              //setPwdError(false);
+              console.log("Trying to log in")
+              //handleClose()
+            }
+            else
+              this.setState({emailError: true})
+              //setPwdError(true);
+      }
+
+      render() {
+        return(
+          <Grid>
+            <Button variant="outlined" color="primary" value={this.state.open} onClick={this.handleOpen}>
+              <AccountCircle />
+              iniciar sesión prueba
+            </Button>
+            <Dialog
+              open ={this.state.open}
+              keepMounted
+              onClose={this.handleClose}
+              aria-labelledby="iniciar_sesion"
+              aria-describedby="alert-dialog-slide-description"
+            >
+              <DialogTitle id="iniciar_sesion">{"Inicio de sesión"}</DialogTitle>
+              <DialogContent>
+              <Grid>
+              <TextField
+                required
+                id="email"
+                name='emailValue'
+                label="Email"
+                value={this.state.emailValue}
+                ref={(input) => this.input = input}
+                onChange={this.handleChange}
+                //error={this.handleError}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              </Grid>
+              <Grid>
+              <TextField
+                required
+                id="password"
+                label="Contraseña"
+                type="password"
+                name='pwdValue'
+                value={this.state.pwdValue}
+                ref={(input) => this.input = input}
+                onChange={this.handleChange}
+                //error={this.handleError}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LockOutlinedIcon />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              </Grid>
+              </DialogContent>
+              <DialogActions>
+                <Button  value={this.state.open} onClick={this.handleClose} color="primary">
+                  Cancelar
+                </Button>
+                <Button onClick={this.get_values} color="primary">
+                  Continuar
+                </Button>
+              </DialogActions>
+              </Dialog>
+            </Grid>
+        )
+      }
+    } 
+      
+    export default Login;
