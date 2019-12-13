48057b8021266bbf88abfd964185327706fbf0b1
Carlos Fernandez Donate <alu0100844665@ull.edu.es>
2019-12-13 13:27:45 +0000

    token encriptado

--- test/App.test.js
+++ test/App.test.js
@@ -2,7 +2,13 @@
 //import ReactDOM from 'react-dom';
 //import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+//it('renders without crashing', () => {
+//  const div = document.createElement('div');
   //ReactDOM.render(<App />, div);
+//});
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
 });
