a2a2c6450ee7f48ab00fc16efc99ccfb4e47b624
Daniel Nuez <alu0100881165@ull.edu.es>
2019-12-28 16:41:48 +0000

    Comprobar si el usuario que se esta registrando ya existe.

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -14,15 +14,43 @@ router.get('/', (req, res) => {
 
 // @route POST user
 router.post('/', (req, res) => {
-  const newUser = new User({
-    nombre: req.body.nombre,
-    password: req.body.password,
-    email: req.body.email
-  });
   console.log(`valor de newuser: ${newUser}`);
-  newUser.save()
-  .then(user => res.json(user))
-  .catch(err => console.log(err));
+  User.find({
+    nombre: req.body.nombre
+  }, (err, prevuser) => {
+    if(err)
+    {
+      console.log(err);
+      // res.end('Error: Error del servidor.');
+      res.end({
+        resultado: false,
+        mensaje: 'Error: error del servidor.'
+      });
+    }
+    else if (prevuser.length > 0)
+    {
+      // res.end('Error: El usuario ya existe.')
+      res.end({
+        resultado: false,
+        mensaje: 'Error: el usuario ya existe.'
+      });
+    }
+    else
+    {
+      const newUser = new User({
+        nombre: req.body.nombre,
+        password: req.body.password,
+        email: req.body.email
+      });
+      newUser.save()
+      .then(user => res.json(user))
+      .catch(err => console.log(err));
+    }
+    res.end({
+      resultado: true,
+      mensaje: 'Registro completado.'
+    });
+  });
 });
 
 module.exports = router;
