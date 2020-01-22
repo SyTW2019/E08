7a5aa8c3a37dac1fd505daa0f14247073b291a75
Daniel Nuez <alu0100881165@ull.edu.es>
2020-01-22 11:59:24 +0000

    Arreglado el indice de los items en el front end

--- src/components/Game.js
+++ src/components/Game.js
@@ -13,131 +13,129 @@ import Logo from '../../public/img/logo_clicker.png';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from "@material-ui/core/styles";
 
-
 const mapStateToProps = (state) => {
-    return {
-        user: state.user,
-        items: state.items,
-        data: state.data,
-        stats: state.stats
-    }
+  return {
+    user: state.user,
+    items: state.items,
+    data: state.data,
+    stats: state.stats
+  }
 }
 
 const mapDispatchToProps = (dipatch) =>{
-    return{
-        //saveStats: stats=> dispatch(saveStats(stats))
-        //userDataFetch = datos => dispatch(userDataFetch(datos))
-    }
+  return{
+    //saveStats: stats=> dispatch(saveStats(stats))
+    //userDataFetch = datos => dispatch(userDataFetch(datos))
+  }
 }
 
-
 class Game extends React.Component{
-    constructor(props){
-        super(props)
+  constructor(props){
+    super(props)
 
-        this.state = {
-            dps: 0,
-            oro: this.props.money,
-            i:0,
-            currentLvl: 1,
-            count:0,
-            clickpower:10,
-
-        }
+    this.state = {
+      dps: 0,
+      oro: this.props.money,
+      i:0,
+      currentLvl: 1,
+      count:0,
+      clickpower:10,
     }
+  }
 
+  componentDidMount = () => {
+    this.gameloop = setInterval(this.gameLoop, 1000)
+    this.handleclick = setInterval(this.updateData, 33)
+  }
 
-    componentDidMount = () => {
-        this.gameloop = setInterval(this.gameLoop, 1000)
-        this.handleclick = setInterval(this.updateData, 33)
+  updateData = () => {
+    this.setState({
+      dps: this.state.dps,
+      oro: this.props.money,
+      i:this.state.i,
+      currentLvl: this.state.currentLvl,
+      clickpower: this.state.clickpower,
+    })
+  }
+
+  calc_dps(){
+    var current_dps = 0;
+    for(var loop = 0; loop < 4; loop++)
+    {
+      var specific_dps = 0;
+      if(this.props.items[loop].cantidad > 0)
+      {
+        specific_dps = this.props.items[loop].cantidad*this.props.items[loop].dps
+        current_dps += specific_dps;
+      }
     }
+    this.setState({
+      dps: current_dps,
+      clickpower: (current_dps >0)?current_dps*0.5: 1,
+    })
+  }
 
-    updateData = () => {
-        this.setState({
-            dps: this.state.dps,
-            oro: this.props.money,
-            i:this.state.i,
-            currentLvl: this.state.currentLvl,
-            clickpower: this.state.clickpower,
-        })
-    }
+  gameLoop = (event) =>{
+    //	console.log('data en game');
+    //	console.log(this.props.items);
+    //	console.log('stats en game');
+    //	console.log(this.props.stats);
 
-    calc_dps(){
-        var current_dps = 0;
-        for(var loop = 0; loop < 4; loop++)
-        {
-            var specific_dps = 0;
-            if(this.props.items[loop].cantidad > 0)
-            {
-                specific_dps = this.props.items[loop].cantidad*this.props.items[loop].dps
-                current_dps += specific_dps;
-            }
-        }
-        this.setState({
-            dps: current_dps,
-            clickpower: (current_dps >0)?current_dps*0.5: 1,
-        })
-    }
+    this.calc_dps();
+    this.setState({
+      i: this.state.i+1
+    })
 
-    gameLoop = (event) =>{
-//	console.log('data en game');
-//	console.log(this.props.items);
-//	console.log('stats en game');
-//	console.log(this.props.stats);
-        this.calc_dps();
-        this.setState({
-            i: this.state.i+1
-        })
-//	console.log('i:' + this.state.i);
-    }
+    //	console.log('i:' + this.state.i);
+  }
 
-   
-    render(){
-        var logged = (localStorage.id ? true:false)
-        return(
-            <div id="Game" >
-                <Grid container spacing={2} justify="space-evenly" alignItems="flex-start" >
-                    <Grid item xs={3}>
-                        <Grid container text-align='center'>
-                            {logged === true && (
-                                <Grid>
-                                    <User/>
-                                    <Logout/>
-                                </Grid>
-                            )}
-                            {logged === false && (
-                                <Grid>
-                                    <Login/>
-                                    <Registro/>
-                                </Grid>
-                            )}
-                        </Grid>
-                        <StatList stats_data={{
-                            dps: this.state.dps,
-                            tiempo: this.state.i
-                        }}/>
-                    </Grid>
-                    <Grid item xs={6}>
-                        
-                        <h1>ORO ACTUAL: {this.props.data.money.toFixed(0)} </h1>
-                        <h2>NIVEL ACTUAL: {this.props.data.currentLvl}</h2>
-                        <h3>DPS: {this.state.dps}</h3>
-                        <Mounstro
-                            tiempo = {this.state.i}
-                            dps_data={{
-                                cpower:this.state.clickpower,
-                                current_dps:this.state.dps
-                            }}
-                        />
-                        <img src={Logo} align="middle" alt="logo ull clicker"/>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <ItemList/>
-                    </Grid>
+
+  render(){
+    var logged = (localStorage.id ? true:false)
+    return(
+      <div id="Game" >
+        <Grid container spacing={2} justify="space-evenly" alignItems="flex-start" >
+          <Grid item xs={3}>
+            <Grid container text-align='center'>
+              {logged === true && (
+                <Grid>
+                  <User/>
+                  <Logout/>
                 </Grid>
-            </div>
-        )
-    }
+              )}
+              {logged === false && (
+                <Grid>
+                  <Login/>
+                  <Registro/>
+                </Grid>
+              )}
+            </Grid>
+            <StatList stats_data={{
+              dps: this.state.dps,
+              tiempo: this.state.i
+            }}/>
+          </Grid>
+          <Grid item xs={6}>
+
+            <h1>ORO ACTUAL: {this.props.data.money.toFixed(0)} </h1>
+            <h2>NIVEL ACTUAL: {this.props.data.currentLvl}</h2>
+            <h3>DPS: {this.state.dps}</h3>
+            <Mounstro
+              tiempo = {this.state.i}
+              dps_data={{
+                cpower:this.state.clickpower,
+                current_dps:this.state.dps
+              }}
+            />
+            <img src={Logo} align="middle" alt="logo ull clicker"/>
+          </Grid>
+          <Grid item xs={3}>
+            <ItemList/>
+          </Grid>
+        </Grid>
+      </div>
+    )
+  }
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(Game);
--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ import {userLoginFetch} from '../js/actions/index';
 
 function mapDispatchToProps(dispatch) {
   return {
-      userLoginFetch: user => dispatch(userLoginFetch(user))
+    userLoginFetch: user => dispatch(userLoginFetch(user))
   };
 }
 
@@ -24,120 +24,113 @@ function mapStateToProps(state) {
 
 
 class Login extends React.Component{
+  constructor(props){
+    super(props);
 
-    constructor(props){
-        super(props);
-
-        this.state = {
-            emailValue: "",
-            pwdValue: "",
-            open: false,
-            emailError: false,
-            pwdError: false,
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleError = this.handleError.bind(this);
-        this.handleOpen = this.handleOpen.bind(this);
-        this.handleClose = this.handleClose.bind(this);
+    this.state = {
+      emailValue: "",
+      pwdValue: "",
+      open: false,
+      emailError: false,
+      pwdError: false,
     }
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleError = this.handleError.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
 
+  handleChange = event => {
+    event.preventDefault();
 
-    handleChange = event => {
-        event.preventDefault();
-       
-        const target = event.target;
-        const value = target.value;
-        const name = target.name
-        this.setState({
-          [name]: value
-        });
-        this.handleError();
-      }
+    const target = event.target;
+    const value = target.value;
+    const name = target.name
+    this.setState({
+      [name]: value
+    });
+    this.handleError();
+  }
 
-      handleError = event => {
-        if(this.get_value()){
-          this.setState({
-            emailError:false,
-          })
+  handleError = event => {
+    if(this.get_value()){
+      this.setState({
+        emailError:false,
+      })
 
-          event.preventDefault()
-          //this.props.userLoginFetch(this.state)
-          this.props.userLoginFetch({ email: this.state.emailValue,
-                                      contrasena: this.state.pwdValue
-          }).then((success) => {
-		          
-             
-              if(localStorage.id === 1)
-                console.log("Usuario logeado correctamente");
-              else if(localStorage.id === 2)
-              {
-                console.log("Contraseña mal puesta")
-              }
-              else if(localStorage.id === 0)
-                console.log("Email mal");
-	            else
-      	        console.log("Error desconocido");
+      event.preventDefault()
+      //this.props.userLoginFetch(this.state)
+      this.props.userLoginFetch({
+        email: this.state.emailValue,
+        contrasena: this.state.pwdValue
+      })
+      .then((success) => {
 
-	            })
-        }
-        else
-          this.setState({
-            emailError:true,
-          })
-      }
+        if(localStorage.id === 1)
+          console.log("Usuario logeado correctamente");
+          else if(localStorage.id === 2)
+          {
+            console.log("Contraseña mal puesta")
+          }
+          else if(localStorage.id === 0)
+            console.log("Email mal");
+	        else
+            console.log("Error desconocido");
+	    })
+    }
+    else
+      this.setState({
+        emailError:true,
+      })
+  }
 
-      handleOpen = event => {
-        this.setState({
-          open: true,
-        })
+  handleOpen = event => {
+    this.setState({
+      open: true,
+    })
+  }
 
+  handleClose = event => {
+    this.setState({
+      open: false,
+    })
+  }
 
+  validateEmail(){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.emailValue))
+      return true;
+    else
+      return false;
+  }
 
-      }
-      handleClose = event => {
-        this.setState({
-          open: false,
-        })
-      }
+  get_value(){
+    if(this.validateEmail())
+    {
+      if(this.state.emailValue)
+        return true;
+      else
+        return false
+    }
+  }
 
-      validateEmail(){
-        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.emailValue))
-          return true;
-        else
-          return false;
-      }
-
-      get_value(){
-
-
-        if(this.validateEmail())
-        {
-          if(this.state.emailValue)
-            return true;
-          else
-            return false
-        }
-      }
-
-      render() {
-        return(
-          <Grid>
-            <Button variant="outlined" color="primary" value={this.state.open} onClick={this.handleOpen}>
-              <AccountCircle />
-              iniciar sesión
-            </Button>
-            <Dialog
-              open={this.state.open}
-              keepMounted
-              onClose={this.handleClose}
-              aria-labelledby="iniciar_sesion"
-              aria-describedby="alert-dialog-slide-description"
-            >
-              <DialogTitle id="iniciar_sesion">{"Inicio de sesión"}</DialogTitle>
-              <DialogContent>
-              <Grid>
+  render() {
+    return(
+      <Grid>
+        <Button variant="outlined" color="primary" value={this.state.open} onClick={this.handleOpen}>
+          <AccountCircle />
+            iniciar sesión
+        </Button>
+        <Dialog
+          open={this.state.open}
+          keepMounted
+          onClose={this.handleClose}
+          aria-labelledby="iniciar_sesion"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle id="iniciar_sesion">{"Inicio de sesión"}</DialogTitle>
+            <DialogContent>
+            <Grid>
               <TextField
                 required
                 id="email"
